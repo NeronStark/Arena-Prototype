bfe155bd3b1cd6cf1fb1d03dc0a484845fc3d24c
Neron94 <neron_venom@mail.ru>
2019-05-01 22:05:20 +0500

    Добавил Меню Стройка, Кухня, Вилла, Воины

---
+++ Core/LudusData.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LudusData : MonoBehaviour
+{
+    public Text coin;
+    public Text food;
+    public Text influence;
+
+    public int servantsMax;
+    public int servantsHave;
+
+    public int coinMax;
+    public int coinCur;
+
+    public int numberTurn;
+
+    public int cornMax;
+    public int cornCur;
+
+    public int foodMax;
+    public int foodCur;
+
+    public void Start()
+    {
+        foodMax = 100;
+        coinMax = 300;
+        ChangeResource(100,"coin");
+        ChangeResource(30,"food");
+    }
+
+    public void ChangeResource(int value, string type)
+    {
+        switch (type)
+        {
+            case "servant":
+                if (servantsHave + value <= servantsMax) servantsHave = servantsHave + value;
+                else servantsHave = servantsMax;
+                break;
+            case "coin":
+                if (coinCur + value <= coinMax) coinCur = coinCur + value;
+                else coinCur = coinMax;
+                coin.text = coinCur+"";
+                break;
+            case "corn":
+                if (cornCur + value <= cornMax) cornCur = cornCur + value;
+                else cornCur = cornMax;
+                break;
+            case "food":
+                if (foodCur + value <= foodMax) foodCur = foodCur + value;
+                else foodCur = foodMax;
+                food.text = foodCur+"";
+                break;
+            case "turn":
+                numberTurn += value;
+                break;
+        }
+    }
+}
---
+++ Core/UIMenu.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMenu : MonoBehaviour
+{
+
+    public LudusData _LudusData;
+    public GameObject menu;
+    public GameObject fieldMenu;
+    public GameObject buildMenu;
+    public GameObject kitchenMenu;
+    public GameObject villaMenu;
+    public GameObject warriorMenu;
+    public GameObject slaveMenu;
+    public GameObject warehouseMenu;
+
+    public List<GameObject> Menus;
+
+
+    public void MenuAction(string type)
+    {
+        
+        switch(type)
+        {
+            case "field":
+                menu.SetActive(true);
+                fieldMenu.SetActive(true);
+                break;
+
+            case "build":
+                menu.SetActive(true);
+                buildMenu.SetActive(true);
+                break;
+
+            case "kitchen":
+                menu.SetActive(true);
+                kitchenMenu.SetActive(true);
+                break;
+
+            case "villa":
+                menu.SetActive(true);
+                villaMenu.SetActive(true);
+                break;
+
+            case "warrior":
+                menu.SetActive(true);
+                warriorMenu.SetActive(true);
+                break;
+
+            case "slave":
+                menu.SetActive(true);
+                slaveMenu.SetActive(true);
+                break;
+
+            case "warehouse":
+                menu.SetActive(true);
+                warehouseMenu.SetActive(true);
+                break;
+
+            case "close":
+                foreach(GameObject menu in Menus)
+                {
+                    menu.SetActive(false);
+                }
+                menu.SetActive(false);
+                break;
+        }
+    }
+
+    
+    
+}
+
+    
